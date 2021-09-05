855ef0d3aa586121ed5552d4a06871e964ed575d
Lokeswari Satyanarayana <lokeswari.satyanarayana@mutualmobile.com>
2021-09-05 18:49:09 +0530

    Initial Commit

---
+++ TinderAnimation/TinderAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAnimationApp.swift
+//  TinderAnimation
+//
+//  Created by Lokeswari Satyanarayana on 05/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TinderAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
